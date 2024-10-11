c1c666b5838fcee44c27a633d0de0f3780caf174
antony-lucide <113905621+antony-lucide@users.noreply.github.com>
2024-10-11 10:25:26 +0200

    Category

--- src/views/Category.php
+++ src/views/Category.php
@@ -2,42 +2,50 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="styles.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Category</title>
+    <link rel="stylesheet" href="../../assets/css/styles.css">
 </head>
-    <body>
-        <section class="dropdown">
-            <span>🔎</span>
-            <section class="dropdown-content">
-                <a href="http://">License🎟</a>
-                <a href="http://">Prix💰</a>
-            </section>
+<body>
+    <section class="dropdown">
+        <span>🔎</span>
+        <section class="dropdown-content">
+            <a href="http://">License 🎟</a>
+            <a href="http://">Prix 💰</a>
         </section>
-        <section class="container">
-            <section class="articles">
-                <img src="../../assets/images/placeholder.png" alt="" srcset="" width='20%' height='30%'>
-                    <button type="button">🔲</button>
-                    <button type="button">🔲</button>
-                    <button type="button">🔲</button>
-            </section>
-            <section class="articles">
-                <img src="../../assets/images/placeholder.png" alt="" srcset="" width='20%' height='30%'>
-            </section>
-            <section class="articles">
-                <img src="../../assets/images/placeholder.png" alt="" srcset="" width='20%' height='30%'>
-            </section>
-            <section class="articles">
-                <img src="../../assets/images/placeholder.png" alt="" srcset="" width='20%' height='30%'>
-            </section>
-            <section class="articles">
-                <img src="../../assets/images/placeholder.png" alt="" srcset="" width='20%' height='30%'>
-            </section>
+    </section>
+
+    <section class="container">
+        <section class="articles">
+            <img src="../../assets/images/placeholder.png" alt="Article Image">
+            <ul>
+                <li><button type="button">🔲</button></li>
+                <li><button type="button">🔲</button></li>
+                <li><button type="button">🔲</button></li>
+            </ul>
         </section>
-        <section class="chapters">
-            <a href="http://" target="_blank" rel="noopener noreferrer">1</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">2</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">3</a>
+
+        <section class="articles">
+            <img src="../../assets/images/placeholder.png" alt="Article Image">
         </section>
-    </body>
+
+        <section class="articles">
+            <img src="../../assets/images/placeholder.png" alt="Article Image">
+        </section>
+
+        <section class="articles">
+            <img src="../../assets/images/placeholder.png" alt="Article Image">
+        </section>
+
+        <section class="articles">
+            <img src="../../assets/images/placeholder.png" alt="Article Image">
+        </section>
+    </section>
+
+    <section class="chapters">
+        <a href="http://">1</a>
+        <a href="http://">2</a>
+        <a href="http://">3</a>
+    </section>
+</body>
 </html>
