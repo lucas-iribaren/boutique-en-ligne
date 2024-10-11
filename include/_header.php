649ca5e7ea8a2bdcdac5b191beb1e8d4fd7b4d11
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-10-11 14:44:32 +0200

    changement des chemins des assets pour que la page 'admin-produits' puissent bien inclure tous les fichiers php : head, header, bandeau, footer

--- include/_header.php
+++ include/_header.php
@@ -4,12 +4,12 @@
         <ul>
             <li>
                 <a class="#" href="#">Panier
-                    <img class="hw-50px" src="assets/images/panier.png" alt="panier logo">
+                    <img class="hw-50px" src="../../assets/images/panier.png" alt="panier logo">
                 </a>
             </li>
             <li>
                 <a href="#">Rechercher
-                    <img class="hw-50px" src="assets/images/loupe.png" alt="loupe logo">
+                    <img class="hw-50px" src="../../assets/images/loupe.png" alt="loupe logo">
                 </a>
             </li>
         </ul>
@@ -23,17 +23,17 @@
         </div>
     </nav>
     <a href="index.php">
-        <img src="assets/images/logo.png" class="logo" alt="logo">
+        <img src="../../assets/images/logo.png" class="logo" alt="logo">
     </a>
     <h1 class="hide_mobile">Pixel Plush</h1>
     <nav class="flex space-center vertical-center gap">
         <input class="hide_mobile" type="text" placeholder="Rechercher">
         <a class="hide_mobile" href="index.php?page=panier">
-            <img class="hw-50px" src="assets/images/panier.png" alt="panier logo">
+            <img class="hw-50px" src="../../assets/images/panier.png" alt="panier logo">
             Panier
         </a>
         <a class="flex column vertical-center" href="#">
-            <img class="hw-50px" src="assets/images/utilisateur.png" alt="utilisateur logo">
+            <img class="hw-50px" src="../../assets/images/utilisateur.png" alt="utilisateur logo">
             <?php
             if (isset($_SESSION['user'])) {
                 echo "Bonjour" . $_SESSION['user']['prenom'];
--- src/views/admin-produits.php
+++ src/views/admin-produits.php
@@ -1,1 +1,9 @@
-<main></main>
+<?php
+// Définition du chemin correct vers le dossier 'include'
+$includePath = __DIR__ . '/../../include/';
+
+// Inclusion des fichiers
+include $includePath . '_head.php';
+include $includePath . '_header.php';
+include $includePath . '_bandeau.php';
+include $includePath . '_footer.php';
