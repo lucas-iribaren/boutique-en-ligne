52c2881e9bdd3917842a0d29a2531e7f671d41e1
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-10-14 11:48:16 +0200

    Modification des données des cellules en les effacant sauf la 1ère ligne remplacée par les titres de chaque colonne

--- src/views/admin-produits.php
+++ src/views/admin-produits.php
@@ -14,45 +14,45 @@ include $includePath . '_header.php';
         
         <!-- Tableau -->
         <table class="product-table">
-            <!-- Ligne 1 -->
+            <!-- Ligne 1 : En-têtes -->
             <tr>
-                <td>Cellule 1</td>
-                <td>Cellule 2</td>
-                <td>Cellule 3</td>
-                <td>Cellule 4</td>
-                <td>Cellule 5</td>
-                <td>Cellule 6</td>
-                <td>Cellule 7</td>
+                <td>ID</td>
+                <td>Nom produit</td>
+                <td class="description-header">Description</td>
+                <td>Prix</td>
+                <td>Quantité</td>
+                <td>Sous-catégorie</td>
+                <td>Catégorie</td>
             </tr>
-            <!-- Ligne 2 -->
+            <!-- Ligne 2 : Vide -->
             <tr>
-                <td>Cellule 8</td>
-                <td>Cellule 9</td>
-                <td>Cellule 10</td>
-                <td>Cellule 11</td>
-                <td>Cellule 12</td>
-                <td>Cellule 13</td>
-                <td>Cellule 14</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
-            <!-- Ligne 3 -->
+            <!-- Ligne 3 : Vide -->
             <tr>
-                <td>Cellule 15</td>
-                <td>Cellule 16</td>
-                <td>Cellule 17</td>
-                <td>Cellule 18</td>
-                <td>Cellule 19</td>
-                <td>Cellule 20</td>
-                <td>Cellule 21</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
-            <!-- Ligne 4 -->
+            <!-- Ligne 4 : Vide -->
             <tr>
-                <td>Cellule 22</td>
-                <td>Cellule 23</td>
-                <td>Cellule 24</td>
-                <td>Cellule 25</td>
-                <td>Cellule 26</td>
-                <td>Cellule 27</td>
-                <td>Cellule 28</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
         </table>
     </section>
