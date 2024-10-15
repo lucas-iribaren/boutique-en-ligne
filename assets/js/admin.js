086e9b2c0aa5b24c467146e24482abb54f9f5861
antony-lucide <113905621+antony-lucide@users.noreply.github.com>
2024-10-15 13:47:42 +0200

    Add Image

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,34 +1,38 @@
 window.onload = function(){
+    console.log('test')
 
-document.getElementById('clickable-image').addEventListener('click', function () {
-    document.getElementById('image-upload').click()
-});
+    document.getElementById('clickable-image').addEventListener('click', function () {
+        document.getElementById('image-upload').click();
+    });
 
-document.getElementById('image-upload').addEventListener('change', function (event) {
-    const file = event.target.files[0]
-    if (file) {
-        const reader = new FileReader()
-        reader.onload = function (e) {
-            document.getElementById('clickable-image').src = e.target.result
-        };
-        reader.readAsDataURL(file)
-    }
-   })
+    document.getElementById('image-upload').addEventListener('change', function (event) {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                document.getElementById('clickable-image').src = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+
+
 
    async function AddDb(){
-    fetch('admin-sub-category', {
-        method: 'POST', 
-        headers: {
-            'Content-Type': 'application/json', 
-        },
-        body: JSON.stringify(data) 
+    const formData = new FormData();
+
+    fetch('admin-sub-category.php', {
+        method: 'POST',
+        body: formData,
     })
-    .then(response => response.json()) 
+    .then(response => response.text())
     .then(data => {
-        console.log('Success:', data); 
+        console.log('Response from server:', data);
     })
-    .catch((error) => {
-        console.error('Error:', error); 
+    .catch(error => {
+        console.error('Error:', error);
     });
-    }
+   }
+   
+console.log('test')
 }
