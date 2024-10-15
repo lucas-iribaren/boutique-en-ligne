bfe2992b621ed943abcedd12765782742f4499a9
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-10-15 14:13:19 +0200

    Refactor CSS styles and update homepage
    - Update the title of the homepage to "Pixel Plush - Accueil"
    - Replace the placeholder image with a new image for the featured product
    - Remove unnecessary carousel captions and links for the featured products
    - Add a new card for the "Films et Séries" category
    
    Closes #123

--- src/views/index.php
+++ src/views/index.php
@@ -1,54 +1,54 @@
 <?php
-$title = 'Accueil';
+$title = 'Pixel Plush - Accueil';
 
 ob_start();
 ?>
 
 <section class="section_phare">
     <h3>Produits phares</h3>
-    <article class="article_phare flex space-around">
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="product-details">
-                        <img class="" src="./assets/images/placeholder.png" alt="Produit 1">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>Produit 1</h5>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
-                            <a href="?page=produit1" class="btn btn-primary">Voir plus</a>
-                        </div>
+    <article class="article_phare flex space-around carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="product-details">
+                    <img src="./assets/images/peluche.png" alt="Produit 1">
+                    <div class="carousel-caption">
+                        <h5>
+                            Produit 1
+                        </h5>
+                        <p>
+                            Description du produit 1
+                        </p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                <div class="product-details">
+                    <img src="./assets/images/placeholder.png" alt="Produit 1">
+                    <div class="carousel-caption">
+                        <h5>
+                        </h5>
+                        <p>
+                        </p>
                     </div>
                 </div>
                 <div class="carousel-item">
                     <div class="product-details">
-                        <img class="d-block w-100" src="./assets/images/placeholder.png" alt="Produit 2">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>Produit 2</h5>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
-                            <a href="?page=produit2" class="btn btn-primary">Voir plus</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="product-details">
-                        <img class="d-block w-100" src="./assets/images/placeholder.png" alt="Produit 3">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>Produit 3</h5>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
-                            <a href="?page=produit3" class="btn btn-primary">Voir plus</a>
+                        <img src="./assets/images/placeholder.png" alt="Produit 3">
+                        <div class="carousel-caption">
+                            <h5>
+                            </h5>
+                            <p>
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <a class="carousel-control-prev" href="#carouselPharePrev" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
             </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="sr-only">Next</span>
+            <a class="carousel-control-next" href="#carouselPhareNext" role="button" data-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
             </a>
-        </div>
+            <a href="#carouselPhareDetail" class="carousel-control-product"></a>
     </article>
 </section>
 <section class="section_categorie">
@@ -60,5 +60,5 @@ ob_start();
         <a href="#" class="card_categorie">
             <img src="assets/images/films_&_series.png" class="" alt="Catégorie Films et Séries">
         </a>
-    </article>                
+    </article>
 </section>
