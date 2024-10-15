83b0d9a6c83a580fbec062d1bc6f8f4879b3e520
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-10-15 16:00:39 +0200

    Refactor CSS update category cards

--- src/views/index.php
+++ src/views/index.php
@@ -21,18 +21,8 @@ ob_start();
                     </div>
                 </div>
                 <div class="carousel-item">
-                <div class="product-details">
-                    <img src="./assets/images/placeholder.png" alt="Produit 1">
-                    <div class="carousel-caption">
-                        <h5>
-                        </h5>
-                        <p>
-                        </p>
-                    </div>
-                </div>
-                <div class="carousel-item">
                     <div class="product-details">
-                        <img src="./assets/images/placeholder.png" alt="Produit 3">
+                        <img src="./assets/images/placeholder.png" alt="Produit 1">
                         <div class="carousel-caption">
                             <h5>
                             </h5>
@@ -40,24 +30,35 @@ ob_start();
                             </p>
                         </div>
                     </div>
+                    <div class="carousel-item">
+                        <div class="product-details">
+                            <img src="./assets/images/placeholder.png" alt="Produit 3">
+                            <div class="carousel-caption">
+                                <h5>
+                                </h5>
+                                <p>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselPharePrev" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            </a>
-            <a class="carousel-control-next" href="#carouselPhareNext" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            </a>
-            <a href="#carouselPhareDetail" class="carousel-control-product"></a>
+                <a class="carousel-control-prev" href="#carouselPharePrev" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                </a>
+                <a class="carousel-control-next" href="#carouselPhareNext" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                </a>
+                <a href="#carouselPhareDetail" class="carousel-control-product"></a>
     </article>
 </section>
 <section class="section_categorie">
-    <h3>Catégorie</h3>
-    <article class="article_categorie flex space-around flex-wrap">
-        <a href="#" class="card_categorie">
+    <article class="article_categorie flex space-around flex-wrap ">
+        <a href="#" class="card_categorie flex-column">
+            <h3>Catégorie</h3>
             <img src="assets/images/jeux_videos.png" class="" alt="Catégorie jeux video">
         </a>
-        <a href="#" class="card_categorie">
+        <a href="#" class="card_categorie flex-column">
+            <h3>Catégorie</h3>
             <img src="assets/images/films_&_series.png" class="" alt="Catégorie Films et Séries">
         </a>
     </article>
