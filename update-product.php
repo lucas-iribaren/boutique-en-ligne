307601c3d620fda2aac692fa967dcb951c9aefb6
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-10-18 13:50:18 +0200

    Suppression du fichier 'update-product' en trop

--- update-product.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'src/models/ModelProduit.php';
-
-$data = json_decode(file_get_contents('php://input'), true);
-
-$modelProduit = new ModelProduit();
-$success = $modelProduit->updateProduct($data['id'], $data);
-
-echo json_encode(['success' => $success]);
