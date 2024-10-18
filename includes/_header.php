09111635bb561e7fab65c07b82f257c61b7f1cb8
YoussefGhollamallah <youssef.gh@live.fr>
2024-10-18 10:23:21 +0200

    création des includes pour éviter la répétition

---
+++ includes/_header.php
@@ -0,0 +1,10 @@
+<header>
+    <h1>Pixel Plush</h1>
+    <nav>
+        <ul>
+            <li><a href="index">Home</a></li>
+            <li><a href="inscription">Inscription</a></li>
+            <li><a href="index.php?page=connexion">Connexion</a></li>
+        </ul>
+    </nav>
+</header>
