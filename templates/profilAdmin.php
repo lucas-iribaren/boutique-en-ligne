3c1a8a5ee55aa9fe2975f3318801e985e59a4ac5
YoussefGhollamallah <youssef.gh@live.fr>
2024-10-18 16:55:25 +0200

    mise en place du page admin pour test la différenciation des roles

---
+++ templates/profilAdmin.php
@@ -0,0 +1,19 @@
+<?php
+
+if(session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+
+<main>
+    <h1>Profil Admin</h1>
+    <p>
+        <?php
+        if(isset($_SESSION['user'])) {
+            echo "Bonjour " . $_SESSION['user']['prenom'] . " " . $_SESSION['user']['nom'] . "<br>";
+            echo "Email: " . $_SESSION['user']['email'];
+        } else {
+            echo "Vous n'êtes pas connecté.";
+        }
+        ?>
+</main>
